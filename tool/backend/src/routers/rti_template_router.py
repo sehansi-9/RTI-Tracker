ac92511495f450119f77ed6bd6aff66ba39f3877
ChanukaUOJ <chamithroxmpc@gmail.com>
2026-04-08 15:42:27 +0530

    enable authorization for the endpoint

--- tool/backend/src/routers/rti_template_router.py
+++ tool/backend/src/routers/rti_template_router.py
@@ -15,7 +15,7 @@ async def get_rti_templates_endpoint(
     page: int = Query(1, ge=1, description="page number"),
     page_size: int = Query(10, ge=1, le=100, description="page size"),
     service: RTITemplateService = Depends(get_rti_template_service),
-    # user: User = Depends(RoleChecker([UserRole.ADMIN, UserRole.USER])) #uncomment to enable authorization
+    user: User = Depends(RoleChecker([UserRole.ADMIN, UserRole.USER])) #uncomment to enable authorization
     ):
     response = service.get_rti_templates(page=page, page_size=page_size)
     return response
